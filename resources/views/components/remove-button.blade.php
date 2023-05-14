ba8e7c3589c5387087f3f3b182a0345b620a3fd6
Daniel <danielpunk4@gmail.com>
2023-05-14 05:12:41 -0500

    mejoro elementos

--- resources/views/components/remove-button.blade.php
+++ resources/views/components/remove-button.blade.php
@@ -10,7 +10,7 @@
     data-drawer-placement="right"
     class="flex space-x-2 items-center text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded text-sm px-5 py-2.5 text-center">
     <x-heroicon-o-trash class="w-4 h-4"/>
-    <span>Eliminar producto</span>
+    <span>Eliminar</span>
 </button>
 
 
