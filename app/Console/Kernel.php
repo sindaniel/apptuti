2767eabb8fc7f6a6c810e690394c6b7a83077088
Daniel <danielpunk4@gmail.com>
2024-04-15 13:47:36 -0500

    Add scheduled task to run 'app:get-token' command every 20 minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        //comand every 20 minutes
+        $schedule->command('app:get-token')->everyTwentyMinutes();
     }
 
     /**
