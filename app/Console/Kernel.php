a2adebcf2bc0f3b233ed8534738fa5c62b2cfd9c
Daniel <danielpunk4@gmail.com>
2024-08-20 17:32:08 -0500

    Update scheduling in Kernel.php to include hourly command and a command every 20 minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('inspire')->hourly();
         //comand every 20 minutes
         $schedule->command('app:get-token')->cron('*/20 * * * *');
     }
